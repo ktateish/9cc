24f8a6d0e554bb1db3e602c02c673836b81df6a1
Katsuyuki Tateishi <kt@wheel.jp>
2019-06-05 22:06:42 +0900

    Add Vecgor type

--- 9cc.c
+++ 9cc.c
@@ -2,6 +2,30 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// Vector
+typedef struct {
+	void **data;
+	int capacity;
+	int len;
+} Vector;
+
+Vector *new_vector() {
+	Vector *vec = malloc(sizeof(Vector));
+	vec->data = malloc(sizeof(void *) * 16);
+	vec->capacity = 16;
+	vec->len = 0;
+	return vec;
+}
+
+void vec_push(Vector *vec, void *elem) {
+	if (vec->capacity == vec->len) {
+		vec->capacity *= 2;
+		vec->data = realloc(vec->data, sizeof(void *) * vec->capacity);
+	}
+	vec->data[vec->len++] = elem;
+}
 
 // token types
 enum TokenType {
@@ -310,10 +334,38 @@ void gen(Node *node) {
 	printf("  push rax\n");
 }
 
+// Test
+void expect(int line, int expected, int actual) {
+	if (expected == actual) return;
+	fprintf(stderr, "%d: %d expected, but got %d\n", line, expected,
+		actual);
+	exit(1);
+}
+
+void runtest() {
+	Vector *vec = new_vector();
+	expect(__LINE__, 0, vec->len);
+
+	for (int i = 0; i < 100; i++) vec_push(vec, (void *)i);
+
+	expect(__LINE__, 100, vec->len);
+	expect(__LINE__, 0, (long)vec->data[0]);
+	expect(__LINE__, 50, (long)vec->data[50]);
+	expect(__LINE__, 99, (long)vec->data[99]);
+
+	printf("OK\n");
+}
+
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s <number>\n", argv[0]);
+		fprintf(stderr, "Usage: %s -test\n", argv[0]);
 		return 1;
+	}
+
+	if (strcmp(argv[1], "-test") == 0) {
+		runtest();
+		return 0;
 	}
 
 	// tokenize
