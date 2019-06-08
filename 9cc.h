5c327f553f250c7044058aafd6fb23daec01ab26
Katsuyuki Tateishi <kt@wheel.jp>
2019-06-08 09:14:45 +0900

    Add Map

--- 9cc.h
+++ 9cc.h
@@ -12,6 +12,16 @@ typedef struct {
 Vector *new_vector();
 void vec_push(Vector *vec, void *elem);
 
+// Map
+typedef struct {
+	Vector *keys;
+	Vector *vals;
+} Map;
+
+Map *new_map();
+void map_put(Map *map, char *key, void *val);
+void *map_get(Map *map, char *key);
+
 //
 // Tokenizer
 //
--- container.c
+++ container.c
@@ -1,6 +1,7 @@
 // vim:set filetype=c tabstop=8 shiftwidth=8 noexpandtab:
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "9cc.h"
 
@@ -21,6 +22,30 @@ void vec_push(Vector *vec, void *elem) {
 	vec->data[vec->len++] = elem;
 }
 
+// Map
+Map *new_map() {
+	Map *map = malloc(sizeof(Map));
+	map->keys = new_vector();
+	map->vals = new_vector();
+	return map;
+}
+
+void map_put(Map *map, char *key, void *val) {
+	char *k = malloc(strlen(key) + 1);
+	strcpy(k, key);
+	vec_push(map->keys, k);
+	vec_push(map->vals, val);
+}
+
+void *map_get(Map *map, char *key) {
+	for (int i = map->keys->len - 1; 0 <= i; i--) {
+		if (strcmp(map->keys->data[i], key) == 0) {
+			return map->vals->data[i];
+		}
+	}
+	return NULL;
+}
+
 // Test
 void expect(int line, int expected, int actual) {
 	if (expected == actual) return;
@@ -29,7 +54,7 @@ void expect(int line, int expected, int actual) {
 	exit(1);
 }
 
-void runtest() {
+void test_vector() {
 	Vector *vec = new_vector();
 	expect(__LINE__, 0, vec->len);
 
@@ -39,6 +64,24 @@ void runtest() {
 	expect(__LINE__, 0, (long)vec->data[0]);
 	expect(__LINE__, 50, (long)vec->data[50]);
 	expect(__LINE__, 99, (long)vec->data[99]);
+}
 
+void test_map() {
+	Map *map = new_map();
+	expect(__LINE__, 0, (long)map_get(map, "foo"));
+
+	map_put(map, "foo", (void *)2);
+	expect(__LINE__, 2, (long)map_get(map, "foo"));
+
+	map_put(map, "bar", (void *)4);
+	expect(__LINE__, 4, (long)map_get(map, "bar"));
+
+	map_put(map, "foo", (void *)6);
+	expect(__LINE__, 6, (long)map_get(map, "foo"));
+}
+
+void runtest() {
+	test_vector();
+	test_map();
 	printf("OK\n");
 }
