9c55eb33f9cb192172552100ccf3f6c80caca2f1
Katsuyuki Tateishi <kt@wheel.jp>
2019-06-14 12:19:05 +0900

    Separate parser and tokenizer

---
+++ tokenize.c
@@ -0,0 +1,173 @@
+// vim:set filetype=c tabstop=8 shiftwidth=8 noexpandtab:
+#define _POSIX_C_SOURCE 200809L
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "9cc.h"
+
+// token
+Token *new_token(int ty, char *input) {
+	Token *t = malloc(sizeof(Token));
+	t->ty = ty;
+	t->input = input;
+	return t;
+}
+
+Token *new_token_num(int val, char *input) {
+	Token *t = malloc(sizeof(Token));
+	t->ty = TK_NUM;
+	t->val = val;
+	t->input = input;
+	return t;
+}
+
+Token *new_token_ident(char *input, int len) {
+	Token *t = malloc(sizeof(Token));
+	t->ty = TK_IDENT;
+	t->name = strndup(input, len);
+	t->input = input;
+	return t;
+}
+
+// input
+char *user_input;
+
+// tokenized results
+Vector *token_vec;
+
+void init_tokens() { token_vec = new_vector(); }
+
+void push_token(Token *t) { vec_push(token_vec, t); }
+
+Token *tokens(int i) { return token_vec->data[i]; }
+
+// function for reporting an error
+void error(char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+// function for reporting error location
+void error_at(char *loc, char *msg) {
+	int pos = loc - user_input;
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos, "");
+	fprintf(stderr, "^ %s\n", msg);
+	exit(1);
+}
+
+int is_alpha(int c) {
+	return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || (c == '_');
+}
+
+int is_alnum(int c) {
+	return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') ||
+	       ('0' <= c && c <= '9') || (c == '_');
+}
+
+// tokenize a string pointed by user_input and save them to tokens
+void tokenize(char *p) {
+	user_input = p;
+
+	while (*p) {
+		// skip whitespace
+		if (isspace(*p)) {
+			p++;
+			continue;
+		}
+
+		if (strncmp(p, "if", 2) == 0 && !is_alnum(p[2])) {
+			push_token(new_token(TK_IF, p));
+			p += 2;
+			continue;
+		}
+
+		if (strncmp(p, "else", 4) == 0 && !is_alnum(p[4])) {
+			push_token(new_token(TK_ELSE, p));
+			p += 4;
+			continue;
+		}
+
+		if (strncmp(p, "while", 5) == 0 && !is_alnum(p[5])) {
+			push_token(new_token(TK_WHILE, p));
+			p += 5;
+			continue;
+		}
+
+		if (strncmp(p, "for", 3) == 0 && !is_alnum(p[3])) {
+			push_token(new_token(TK_FOR, p));
+			p += 3;
+			continue;
+		}
+
+		if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])) {
+			push_token(new_token(TK_RETURN, p));
+			p += 6;
+			continue;
+		}
+
+		if (strncmp(p, "int", 3) == 0 && !is_alnum(p[3])) {
+			push_token(new_token(TK_INT, p));
+			p += 3;
+			continue;
+		}
+
+		if (is_alpha(*p)) {
+			char *q = p;
+			while (is_alnum(*q)) q++;
+			int len = q - p;
+
+			push_token(new_token_ident(p, len));
+			p += len;
+			continue;
+		}
+
+		if (*p == '=' && *(p + 1) == '=') {
+			push_token(new_token(TK_EQ, p));
+			p += 2;
+			continue;
+		}
+
+		if (*p == '!' && *(p + 1) == '=') {
+			push_token(new_token(TK_NE, p));
+			p += 2;
+			continue;
+		}
+
+		if (*p == '<' && *(p + 1) == '=') {
+			push_token(new_token(TK_LE, p));
+			p += 2;
+			continue;
+		}
+
+		if (*p == '>' && *(p + 1) == '=') {
+			push_token(new_token(TK_GE, p));
+			p += 2;
+			continue;
+		}
+
+		if (*p == '+' || *p == '-' || *p == '*' || *p == '/' ||
+		    *p == '(' || *p == ')' || *p == '<' || *p == '>' ||
+		    *p == ';' || *p == '=' || *p == '{' || *p == '}' ||
+		    *p == ',') {
+			push_token(new_token(*p, p));
+			p++;
+			continue;
+		}
+
+		if (isdigit(*p)) {
+			push_token(new_token_num(strtol(p, &p, 10), p));
+			continue;
+		}
+
+		error_at(p, "cannot tokenize");
+	}
+
+	push_token(new_token(TK_EOF, p));
+}
